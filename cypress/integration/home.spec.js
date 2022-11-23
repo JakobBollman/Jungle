e66053f35d92ce7ee6e869544df18da614d8031b
JakobBollman <jakob@bollman.ca>
2022-11-23 14:06:05 -0700

    feature/testing-home-page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,18 @@
+describe('', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it("Jungle Visit", () => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
+});
